1653afa8273040ef146b3f75d2a95c69d2183330
Esther <estherajayibunmi@gmail.com>
2023-04-24 09:22:12 -0700

    add 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet using only putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+	putchar('\n');
+	
+	return (0);
+}
