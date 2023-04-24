3add643ad610d794bb43c36f989d5e61dcc10eac
Esther <estherajayibunmi@gmail.com>
2023-04-24 10:12:53 -0700

    add 4-print_alphabt file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-        char lowercase;
-
-        for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	char lowercase;
+	
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
 		if (lowercase != 'e' && lowercase != 'q')
 			putchar(lowercase);
 	}
-        putchar('\n');
-
-        return (0);
+	putchar('\n');
+	
+	return (0);
 }
