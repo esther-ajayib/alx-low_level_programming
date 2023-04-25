d1d906696bd9c6d7be04ccefeb0230f69133c2b7
Esther <estherajayibunmi@gmail.com>
2023-04-25 07:21:26 -0700

    add file 2-alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,10 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++) {
-		for (j = 'a'; j <= 'z'; j++) {
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
 			_putchar(j);
 		}
 		_putchar('\n');
