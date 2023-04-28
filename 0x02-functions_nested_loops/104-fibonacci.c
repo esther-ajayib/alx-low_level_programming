ea503ffabdbd84865e05bc7090e377943879a96e
Esther <estherajayibunmi@gmail.com>
2023-04-28 05:02:37 -0700

    add file 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,12 +7,14 @@
 
 void print_number(int n)
 {
-	if (n < 10) {
-		_putchar(n + '0');
-		return;
+	if (n < 0) {
+		_putchar('-');
+	n = -n;
 	}
 
-	print_number(n / 10);
+	if (n / 10)
+		print_number(n / 10);
+
 	_putchar(n % 10 + '0');
 }
 
