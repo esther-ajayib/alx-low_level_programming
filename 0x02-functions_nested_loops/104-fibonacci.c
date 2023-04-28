86790b6116872f5a258ecc82add8921fbea44b05
Esther <estherajayibunmi@gmail.com>
2023-04-28 04:37:56 -0700

    add file 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+
+void print_number(int n)
+{
+	if (n < 0) {
+	_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
 
 /**
  * main - prints the first 98 Fibonacci numbers
