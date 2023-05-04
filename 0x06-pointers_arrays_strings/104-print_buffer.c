947dab477039fb4020aa7d68a9f53d7e2f77107b
Esther <estherajayibunmi@gmail.com>
2023-05-04 05:16:39 -0700

    My last commit

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_buffer - Prints the content of a buffer
+ * @b: The buffer to print
+ * @size: The size of the buffer
+ *
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < size)
+			{
+				printf("%02x", b[j]);
+			} else
+			{
+				printf("  ");
+			}
+
+			if (j % 2 != 0)
+			{
+				printf(" ");
+			}
+		}
+
+		for (j = i; j < i + 10; j++)
+		{
+			if (j >= size)
+			{
+				break;
+			}
+			else if (*(b + j) >= ' ' && *(b + j) <= '~')
+			{
+				printf("%c", *(b + j));
+			}
+			else
+			{
+				printf(".");
+			}
+		}
+
+		printf("\n");
+	}
+}
+
