01ba86c6ce1fc80b3e6a31caf8e2b407a53e667a
Esther <estherajayibunmi@gmail.com>
2023-05-04 03:28:01 -0700

    My eighth commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @s: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char leet[] = "aAeEoOtTlL";
+	char leet_replace[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; leet[j] != '\0'; j++)
+		{
+			if (s[i] == leet[j])
+			{
+				s[i] = leet_replace[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
