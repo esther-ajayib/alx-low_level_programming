ea27675bf4ca2b52096eaf56af393f0b96db7ab8
Esther <estherajayibunmi@gmail.com>
2023-05-04 03:03:10 -0700

    My fifth commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements of the array
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+}
