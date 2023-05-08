c965318dc499b83ffb2be2ab5049308649e8f8d2
Esther <estherajayibunmi@gmail.com>
2023-05-08 06:58:34 -0700

    Add file 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,13 +7,10 @@
  * @needle: Pointer to the substring to locate
  *
  * Return: Pointer to the beginning of the located substring,
- *         or NULL if the substring is not found
+ * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == '\0')
-		return haystack;
-
 	while (*haystack)
 	{
 		char *h = haystack;
