d103b26799d12a03c78300aaaebd6a620e3a1617
Esther <estherajayibunmi@gmail.com>
2023-05-18 06:44:27 -0700

    My last commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,79 +1,96 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
  * multiply - Multiplies two positive numbers
- * @num1: First number
- * @num2: Second number
+ * @num1: The first number
+ * @num2: The second number
  *
- * Return: 0 on success
+ * Return: Always 0
  */
 int multiply(char *num1, char *num2)
 {
-	int len1 = 0, len2 = 0, i, j, carry;
-	int *result;
-	int res_size;
+	int len1 = 0, len2 = 0, i, j, k, carry, n1, n2, sum, *result;
 
-	while (num1[len1] != '\0')
+	while (num1[len1])
 		len1++;
-	while (num2[len2] != '\0')
+	while (num2[len2])
 		len2++;
 
-	res_size = len1 + len2;
-	result = malloc(res_size * sizeof(int));
+	result = malloc(sizeof(int) * (len1 + len2));
 	if (result == NULL)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	for (i = 0; i < res_size; i++)
+	for (i = 0; i < len1 + len2; i++)
 		result[i] = 0;
 
 	for (i = len1 - 1; i >= 0; i--)
 	{
 		carry = 0;
+		n1 = num1[i] - '0';
+
 		for (j = len2 - 1; j >= 0; j--)
 		{
-			int digit1 = num1[i] - '0';
-			int digit2 = num2[j] - '0';
-			int sum = digit1 * digit2 + result[i + j + 1] + carry;
-			result[i + j + 1] = sum % 10;
+			n2 = num2[j] - '0';
+			sum = n1 * n2 + result[i + j + 1] + carry;
 			carry = sum / 10;
+			result[i + j + 1] = sum % 10;
 		}
-		result[i] += carry;
+
+		if (carry > 0)
+			result[i + j + 1] += carry;
 	}
 
-	i = 0;
-	while (i < res_size - 1 && result[i] == 0)
-		i++;
+	k = 0;
+	while (result[k] == 0 && k < len1 + len2)
+		k++;
 
-	for (; i < res_size; i++)
-		_putchar(result[i] + '0');
+	if (k == len1 + len2)
+		_putchar('0');
+
+	for (; k < len1 + len2; k++)
+		_putchar(result[k] + '0');
+
 	_putchar('\n');
 
 	free(result);
-
 	return (0);
 }
 
+/**
+ * main - Entry point
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	char *num1;
-	char *num2;
+	int i, j;
 
 	if (argc != 3)
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
+		printf("Error\n");
 		return (98);
 	}
 
-	num1 = argv[1];
-	num2 = argv[2];
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (98);
+			}
+		}
+	}
 
-	multiply(num1, num2);
+	multiply(argv[1], argv[2]);
 
 	return (0);
 }
