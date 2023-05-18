e7d05f0b0b9a0f37c25cc6f3fe280a6bc14087b9
Esther <estherajayibunmi@gmail.com>
2023-05-18 05:04:53 -0700

    My last commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Entry point
@@ -7,21 +8,73 @@
  * @argv: Array of command-line arguments
  * Return: 0 on success, 98 on error
  */
-int main(int argc, char **argv)
-{
-	int num1, num2, result;
+int multiply(char *num1, char *num2) {
+	int len1 = 0, len2 = 0, i, j, carry;
+	int *result;
+	int res_size;
 
-	if (argc != 3)
-	{
+	while (num1[len1] != '\0')
+		len1++;
+	while (num2[len2] != '\0')
+		len2++;
+
+	res_size = len1 + len2;
+	result = calloc(res_size, sizeof(int));
+
+	for (i = len1 - 1; i >= 0; i--) {
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--) {
+			int digit1 = num1[i] - '0';
+			int digit2 = num2[j] - '0';
+			int sum = digit1 * digit2 + result[i + j + 1] + carry;
+			result[i + j + 1] = sum % 10;
+			carry = sum / 10;
+		}
+		result[i] += carry;
+	}
+
+	i = 0;
+	while (i < res_size - 1 && result[i] == 0)
+		i++;
+
+	for (; i < res_size; i++)
+		printf("%d", result[i]);
+	printf("\n");
+
+	free(result);
+
+	return (0);
+}
+
+int main(int argc, char *argv[]) {
+	int i;
+
+	char *num1;
+	char *num2;
+
+	if (argc != 3) {
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
+
+	num1 = argv[1];
+	num2 = argv[2];
+
+	for (i = 0; num1[i] != '\0'; i++) {
+		if (!isdigit((unsigned char)num1[i])) {
+			printf("Error\n");
+			return (98);
+		}
 	}
+
+	for (i = 0; num2[i] != '\0'; i++) {
+		if (!isdigit((unsigned char)num2[i])) {
+			printf("Error\n");
+			return (98);
+		}
+	}
+
+	multiply(num1, num2);
+
+	return (0);
 }
