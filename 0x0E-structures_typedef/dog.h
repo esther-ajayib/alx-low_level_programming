4ba6d2d68b4cb148a9b45482dee859f10e2e620e
Esther <estherajayibunmi@gmail.com>
2023-05-22 13:26:54 +0100

    My first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - Structure representing a dog
+ *@name: Pointer to the name of the dog
+ *@age: Age of the dog
+ *@owner: Pointer to the owner of the dog
+ *
+ *Description: This structure represents a dog and its attributes.
+ *
+ */
+
 struct dog {
 	char *name;
 	float age;
