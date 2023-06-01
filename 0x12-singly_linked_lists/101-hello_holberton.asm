f535dbe80de547844bd7f3ef2f50d6445f24cce1
Esther <estherajayibunmi@gmail.com>
2023-06-01 07:18:33 -0700

    Last commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,21 @@
 section .data
-    hello_msg db 'Hello, Holberton', 0x0A ; 0x0A is the ASCII code for a new line
-    fmt db "%s", 0
+	hello_msg db 'Hello, Holberton', 0x0A ; 0x0A is the ASCII code for a new line
+	fmt db "%s", 0
 
 section .text
-    extern printf
+	extern printf
 
 global main
 main:
-    sub rsp, 8     ; Allocate space for 64-bit alignment
+	sub rsp, 8     ; Allocate space for 64-bit alignment
 
-    mov rdi, fmt   ; Format string
-    mov rsi, hello_msg ; Message to print
-    xor eax, eax   ; Clear EAX register
-    call printf    ; Call printf function
+	mov rdi, fmt   ; Format string
+	mov rsi, hello_msg ; Message to print
+	xor eax, eax   ; Clear EAX register
+	call printf    ; Call printf function
 
-    add rsp, 8     ; Restore stack
+	add rsp, 8     ; Restore stack
 
-    mov eax, 0x60  ; Exit syscall number
-    xor edi, edi   ; Exit status code
-    syscall        ; Invoke syscall
+	mov eax, 0x60  ; Exit syscall number
+	xor edi, edi   ; Exit status code
+	syscall        ; Invoke syscall
