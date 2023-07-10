14b2036d84d5b0adbafa3e3bcbeeab42fe1c4443
Esther AJAYI <estheradexdainty@gmail.com>
2023-07-11 00:28:27 +0100

    elf file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,45 +1,73 @@
+#include "elf_header.h"
 #include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <fcntl.h>
+
+/**
+ * print_error - Prints an error message to stderr and exits with status code 98
+ * @msg: The error message to print
+ */
+void print_error(const char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	exit(98);
+}
 
 /**
  * print_elf_header - Prints the information contained in the ELF header
- * @elf_header: Pointer to the ELF header structure
+ * @header: Pointer to the ELF header structure
  */
-void print_elf_header(uint8_t *elf_header)
+void print_elf_header(Elf64_Ehdr *header)
 {
 	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	for (int i = 0; i < 16; i++)
-		printf("%02x ", elf_header[i]);
-	printf("\n");
-	printf("  Class:   %s\n", elf_header[4] == 1 ? "ELF32" : "ELF64");
-	printf("  Data:    %s\n", elf_header[5] == 1 ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version: %d (current)\n", elf_header[6]);
-	printf("  OS/ABI:  UNIX - ");
-	switch (elf_header[7])
+	printf("  Magic:   %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n",
+		   header->e_ident[0], header->e_ident[1], header->e_ident[2], header->e_ident[3],
+		   header->e_ident[4], header->e_ident[5], header->e_ident[6], header->e_ident[7],
+		   header->e_ident[8], header->e_ident[9], header->e_ident[10], header->e_ident[11],
+		   header->e_ident[12], header->e_ident[13], header->e_ident[14], header->e_ident[15]);
+	printf("  Class:                             ELF%d\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ? 32 : 64);
+	printf("  Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n", get_osabi_name(header->e_ident[EI_OSABI]));
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %s\n", get_type_name(header->e_type));
+	printf("  Entry point address:               0x%lx\n", header->e_entry);
+}
+
+/**
+ * get_osabi_name - Returns the name corresponding to the given OS/ABI identifier
+ * @osabi: The OS/ABI identifier
+ *
+ * Return: The name of the OS/ABI
+ */
+const char *get_osabi_name(unsigned char osabi)
+{
+	switch (osabi)
 	{
-		case 0: printf("System V\n"); break;
-		case 1: printf("HP-UX\n"); break;
-		case 2: printf("NetBSD\n"); break;
-		case 3: printf("Linux\n"); break;
-		case 6: printf("Solaris\n"); break;
-		case 9: printf("FreeBSD\n"); break;
-		default: printf("<unknown: %d>\n", elf_header[7]); break;
+		case ELFOSABI_SYSV: return "UNIX - System V";
+		case ELFOSABI_NETBSD: return "UNIX - NetBSD";
+		case ELFOSABI_SOLARIS: return "UNIX - Solaris";
+		default: return "<unknown>";
 	}
-	printf("  ABI Version: %d\n", elf_header[8]);
-	printf("  Type:    ");
-	switch (*(uint16_t *)(elf_header + 16))
+}
+
+/**
+ * get_type_name - Returns the name corresponding to the given ELF type
+ * @type: The ELF type
+ *
+ * Return: The name of the ELF type
+ */
+const char *get_type_name(unsigned int type)
+{
+	switch (type)
 	{
-		case 1: printf("REL (Relocatable file)\n"); break;
-		case 2: printf("EXEC (Executable file)\n"); break;
-		case 3: printf("DYN (Shared object file)\n"); break;
-		case 4: printf("CORE (Core file)\n"); break;
-		default: printf("<unknown: %d>\n", *(uint16_t *)(elf_header + 16)); break;
+		case ET_NONE: return "NONE (No file type)";
+		case ET_REL: return "REL (Relocatable file)";
+		case ET_EXEC: return "EXEC (Executable file)";
+		case ET_DYN: return "DYN (Shared object file)";
+		case ET_CORE: return "CORE (Core file)";
+		default: return "<unknown>";
 	}
-	printf("  Entry point address: 0x%x\n", *(uint32_t *)(elf_header + 24));
 }
 
 /**
@@ -47,34 +75,29 @@ void print_elf_header(uint8_t *elf_header)
  * @filename: The name of the ELF file to analyze
  *
  * This function opens the specified ELF file in binary mode, reads its header,
- * and calls the print_elf_header function to display the information.
- * If the file cannot be opened or an error occurs during reading, an appropriate
- * error message is displayed to stderr.
+ * displays the information contained in the header, such as the magic bytes,
+ * class, data encoding, version, OS/ABI, ABI version, type, and
+ * entry point address. If the file cannot be opened or an error occurs
+ * during reading, an appropriate error message is displayed to stderr.
  *
  * Return: 0 on success, non-zero error code on failure
  */
 int elf_header(const char *filename)
 {
-	int file = open(filename, O_RDONLY);
+	int fd, read_count;
+	Elf64_Ehdr header;
 
-	if (file == -1)
-	{
-		fprintf(stderr, "Error: Unable to open file %s\n", filename);
-		return 98;
-	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		print_error("Error: Unable to open file");
 
-	uint8_t elf_header[32];
-	ssize_t bytes_read = read(file, elf_header, sizeof(elf_header));
-	if (bytes_read != sizeof(elf_header))
-	{
-		fprintf(stderr, "Error: Failed to read ELF header\n");
-		close(file);
-		return 98;
-	}
+	read_count = read(fd, &header, sizeof(header));
+	if (read_count != sizeof(header))
+		print_error("Error: Unable to read ELF header");
 
-	print_elf_header(elf_header);
+	print_elf_header(&header);
 
-	close(file);
+	close(fd);
 	return 0;
 }
 
@@ -82,6 +105,7 @@ int elf_header(const char *filename)
  * main - The entry point for the program to get the header of an ELF file
  * @argc: The number of arguments
  * @argv: The pointer to an array of arguments
+ *
  * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
--- 0x15-file_io/elf_header.h
+++ 0x15-file_io/elf_header.h
@@ -4,5 +4,9 @@
 #include <elf.h>
 
 int elf_header(const char *filename);
+void print_error(const char *msg);
+void print_elf_header(Elf64_Ehdr *header);
+const char *get_osabi_name(unsigned char osabi);
+const char *get_type_name(unsigned int type);
 
 #endif /* ELF_HEADER_H */
