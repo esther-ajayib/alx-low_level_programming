7dbdb88eb063a5dde843e8845c3d1c721558166f
Esther AJAYI <estheradexdainty@gmail.com>
2023-07-10 23:17:06 +0100

    updated files

--- 0x15-file_io/elf_header.h
+++ 0x15-file_io/elf_header.h
@@ -1,43 +1,8 @@
-#ifndef ELF_H
-#define ELF_H
+#ifndef ELF_HEADER_H
+#define ELF_HEADER_H
 
-#include <stdint.h>
+#include <elf.h>
 
-#define EI_NIDENT 16
-#define ELFMAG0 0x7f
-#define ELFMAG1 'E'
-#define ELFMAG2 'L'
-#define ELFMAG3 'F'
+int elf_header(const char *filename);
 
-#define ELFCLASS32 1
-#define ELFCLASS64 2
-
-#define ELFDATA2LSB 1
-#define ELFDATA2MSB 2
-
-#define ET_NONE 0
-#define ET_REL 1
-#define ET_EXEC 2
-#define ET_DYN 3
-#define ET_CORE 4
-
-#define ELFOSABI_SYSV 0
-
-typedef struct {
-    uint8_t e_ident[EI_NIDENT];
-    uint16_t e_type;
-    uint16_t e_machine;
-    uint32_t e_version;
-    uint64_t e_entry;
-    uint64_t e_phoff;
-    uint64_t e_shoff;
-    uint32_t e_flags;
-    uint16_t e_ehsize;
-    uint16_t e_phentsize;
-    uint16_t e_phnum;
-    uint16_t e_shentsize;
-    uint16_t e_shnum;
-    uint16_t e_shstrndx;
-} Elf64_Ehdr;
-
-#endif /* ELF_H */
+#endif /* ELF_HEADER_H */
